b7247ee94d933608ddeb098bc8d3f0e4ab96728e
mpr0xy <mpr0xy@live.com>
2018-09-22 09:07:35 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+    <HeadInfo></HeadInfo>
+  </div>
+</template>
+
+<script>
+import HeadInfo from '@/components/HeadInfo.vue'
+export default {
+  name: 'App',
+  components: {
+    HeadInfo
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
+body {
+  position: relative;
+  max-width: 750px;
+  margin: 0 auto;
+}
+.page {
+  position: relative;
+  max-width: 750px;
+  margin: 0 auto;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HeadInfo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="head-info">
+    <img class="poster" src="http://img0.ph.126.net/t3kXj_tyyoRvUaVB9yCMkQ==/1821987524348614419.jpg" />
+    <p class="title">《大丈夫》火锅酒100元代金券10张</p>
+    <p class="price">¥<span class="now">0</span><span class="old">¥100元</span></p>
+    <hr class="hr">
+    <div class="right-info">
+      <p class="time">2018-09-14 14:00</p>
+      <p>自动开奖</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeadInfo',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
